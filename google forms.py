f0e54a679f905a773ca2bb26df7c3dbf964d1397
Niyas <66733151+niyasnowshad@users.noreply.github.com>
2021-02-08 17:16:41 +0400

    Update google forms.py

--- google forms.py
+++ google forms.py
@@ -1,21 +1,21 @@
 while True:
     from selenium import webdriver
     import random
-
+    #selenium
     option = webdriver.ChromeOptions()
     option.add_argument("-incognito")
     option.add_experimental_option("excludeSwitches", ['enable-automation']);
     # option.add_argument("--headless")
     # option.add_argument("disable-gpu")
     browser = webdriver.Chrome(executable_path='chromedriver', options=option)
-
+    #google forms link
     browser.get("https://forms.gle/ivPCEK9dW7k4K3Uf8")
-
+    #boxes classnames
     textboxes = browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")
     radiobuttons = browser.find_elements_by_class_name("docssharedWizToggleLabeledLabelWrapper")
     checkboxes = browser.find_elements_by_class_name("quantumWizTogglePapercheckboxInnerBox")
     submitbutton = browser.find_element_by_class_name("appsMaterialWizButtonPaperbuttonContent")
-    
+    #name
     file1 = open('names.txt', 'r')
     lines = file1.readlines()
     for i in range(0,25):
@@ -24,6 +24,7 @@ while True:
         textboxes[0].send_keys(x)
         print(x)
         break
+    #multiple choice
     a = random.randint(0, 5)
     radiobuttons[a].click()
     b = random.randint(6, 11)
@@ -34,6 +35,6 @@ while True:
     radiobuttons[d].click()
     e = random.randint(24, 29)
     radiobuttons[e].click()
-
+    #submit button
     submitbutton.click()
     browser.close()
